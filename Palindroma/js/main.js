652080a6acae7fc441f54fcd27be077e28c3b353
Crisano97 <97keb6@gmail.com>
2022-06-22 14:59:15 +0200

    parola palindroma senza commenti

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,7 +1,3 @@
-// chiedere all'utente di inserire una parola;
-// creare una funzione per capire se la parola inserita è palindroma;
-
-//dichiaro la variabile per la parola scritta dall'utente;
 let userWord = prompt('inserisci una parola');
 console.log(userWord); 
 
@@ -14,28 +10,14 @@ if (userWord === palindromeWord) {
     console.log('la tua parola non è palindroma');
 }
 
-
-//creo una funzione per verificare che la parola sia palindroma;
-function reverseString (str) {
-
+function reverseString (string) {
     
-    //uso split() per trasformare la mia stringa in un array;
-    let splitString = str.split("");
-    // console.log(splitString);
-    // es. strano = ['s', 't', 'r', 'a', 'n', 'o'];
-
-    //uso reverse() per capovolgere il mio array e vedere gli elementi in ordine dall'ultimo al primo;
+    let splitString = string.split("");
+    
     let reverseArray = splitString.reverse();
-    // console.log(reverseArray);
-    // es. strano = ['o', 'n', 'a', 'r', 't', 's']
-
-    //uso join() per unire tutti gli elementi dell'array in un'unica stringa;
+    
     let joinArray = reverseArray.join("");
-    // console.log(joinArray);
-    // es. strano = 'onarts';
 
-
-    //ritorno come risultato la stringa capovolta;
     return joinArray;
 
 }
