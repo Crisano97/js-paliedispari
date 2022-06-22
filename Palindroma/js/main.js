619cc87c635f37fe2e06bf14bee40dc8fedba877
Crisano97 <97keb6@gmail.com>
2022-06-22 16:17:59 +0200

    mia soluzione palindromi

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,25 +1,46 @@
-let userWord = prompt('inserisci una parola');
-console.log(userWord); 
+// let userWord = prompt('inserisci una parola');
+// console.log(userWord); 
 
-let palindromeWord = reverseString(userWord);
+// let palindromeWord = reverseString(userWord);
+// console.log(palindromeWord);
+
+// if (userWord === palindromeWord) {
+//     console.log('la tua parola è palindroma');
+// } else {
+//     console.log('la tua parola non è palindroma');
+// }
+
+// function reverseString (string) {
+    
+//     let splitString = string.split("");
+    
+//     let reverseArray = splitString.reverse();
+    
+//     let joinArray = reverseArray.join("");
+
+//     return joinArray;
+
+// }
+
+const userWord = prompt('inserisci una parola');
+console.log(userWord);
+
+const palindromeWord = reverseWord(userWord);
 console.log(palindromeWord);
 
 if (userWord === palindromeWord) {
-    console.log('la tua parola è palindroma');
+    console.log('la parola è palindroma');
 } else {
-    console.log('la tua parola non è palindroma');
+    console.log('la parola non è palindroma');
 }
 
-function reverseString (string) {
-    
-    let splitString = string.split("");
-    
-    let reverseArray = splitString.reverse();
-    
-    let joinArray = reverseArray.join("");
+function reverseWord (word) {
+    let reverseString = " ";
 
-    return joinArray;
+    for (let i = word.length -1; i >= 0 ; i--) {
+        reverseString += word[i];
+        // console.log(reverseString);
+    }
 
+    return reverseString;
 }
-
-
