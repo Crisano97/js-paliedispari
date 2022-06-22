9b1b72590d38e53afed3da4ae8725f329468ea2f
Crisano97 <97keb6@gmail.com>
2022-06-22 16:52:32 +0200

    Pari e Dispari

--- Pari e dispari/js/main.js
+++ Pari e dispari/js/main.js
@@ -1,9 +1,31 @@
 //L'utente sceglie tra pari o dispari;
 //l'utente inserisce un numero da 1 a 5;
+let userChoice = prompt('scegli tra pari o dispari');
+let userNumber = parseInt(prompt('inserisci un numero intero da 1 a 5'));
+console.log(userNumber);
 
 let iaNumber = iaRandomNumber(5, 1);
 console.log(iaNumber);
 
+let sum = userNumber + iaNumber;
+console.log(sum);
+
+isEven(sum);
+
+if (isEven(sum) === true) {
+    console.log('Ha vinto il pari');
+} else {
+    console.log('Ha vinto il dispari');
+}
+
 function iaRandomNumber(max, min) {
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+function isEven(number) {
+    if (number % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
 }
